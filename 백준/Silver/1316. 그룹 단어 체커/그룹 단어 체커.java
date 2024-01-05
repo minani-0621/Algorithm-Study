7492383c4d71175e17b626f78ebd6bc904e4a4ee
조민환 <cm2hcm2h@naver.com>
2024-01-06 02:34:06 +0900

    Update 그룹 단어 체커.java

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
-
+/*
+이 문제 전략 -> 그룹단어인지 아닌지 확인하는 방법으로, 한 단어를 String array[]에 한 단어씩 저장 후, 연달아서 나오는 단어는 한 단어로 취급되도록 새로운 문자열을 ArrayList<String> word에 저장.
+그리고 어레이리스트에 있는 한 단어의 빈도 수를 체크할 수 있는 Collections.frequency()를 사용해서 어레이리스트의 저장된 모든 단어들의 빈도 수를 확인한다. 단 하나의 단어라도 빈도수가 2 이상이 나오면
+그룹단어가 아님을 의미하므로 그룹단어가 아닌 단어갯수를 세는 count 변수를 1씩 증가시킨다. 최종적으로, 전체 입력 단어 수(변수 n) - 그룹 단어가 아닌 단어(변수 count)를 출력하면 된다.
+*/
 public class Main {
 	static Scanner scanner = new Scanner(System.in);
 	static int n;
@@ -39,7 +43,6 @@ public class Main {
 			}
 			loop:
 			for(String str: word) {
-				int total = 0;
 				int frequency = Collections.frequency(word, str);
 				if(frequency >= 2) {
 					count++;
