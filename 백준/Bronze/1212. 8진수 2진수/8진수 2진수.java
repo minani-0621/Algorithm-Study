863a59d95e2d555a71139fe5e91b867726864f3b
조민환 <cm2hcm2h@naver.com>
2024-02-29 15:58:13 +0900

    [Bronze II] Title: 8진수 2진수, Time: 556 ms, Memory: 45216 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class Main {
 	static Scanner scanner = new Scanner(System.in);
 	static StringBuilder binary = new StringBuilder("");
-	// 이 문제에서는 입력된 하나의 8진수를 2진수로 변환하고 차례로 뒤에 이어서 붙여햐 하는데, 이를 다룰 때 int나 long 타입은 처리과정에서 시간복잡도가 너무 커져서 시간초과가 발생하고,
-	// String 객체로 처리하면 String 객체는 한 번 생성되면 그 값을 변경할 수 없고, concat(), append() 함수를 사용하거나, str1 = str2 + str3; 처럼 2진수로 변형된 8진수의 String 값을 이어 붙일 때 마다
-	// 매번 새로운 String 참조변수에 값을 할당해줘야 해서 시간 초과가 발생한다. 이를 해결하기 위해 값을 변경할 수 있는 StringBuilder를 사용해서 처리시간을 단축시킬 수 있다. 
+	
 	public static void main(String[] args) {
 		String octal = scanner.next();
 		int length = octal.length();
