a07a954e91f14d9acfc0b8ccc917f0ad086a38ce
조민환 <cm2hcm2h@naver.com>
2024-03-21 15:32:06 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 2004 ms, Memory: 546240 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -15,13 +15,13 @@ public class Main {
 		
 		HashSet<String> hashset = new HashSet<>();
 		
-		for(int i = 1; i <= length; i++) { // 1 ~ 5
-			for(int j = 0; j < length; j++) { // 
-				if(j + i >= length) {
+		for(int i = 1; i <= length; i++) { // 문자열 길이가 1인것부터 input 스트링의 길이와 동일한 것까지
+			for(int j = 0; j < length; j++) { // input 스트링을 substring()을 적용할 때, 시작 인덱스
+				if(j + i >= length) { // (시작 인덱스 + 찾고자 하는 스트링의 길이) 가 input 스트링의 인덱스 범위를 벗어날 때
 					if(!hashset.contains(input.substring(j))) {
 						hashset.add(input.substring(j));
 					}
-				}else {
+				}else { // (시작 인덱스 + 찾고자 하는 스트링의 길이) 가 input 스트링의 인덱스 범위를 벗어나지 않을 때
 					if(!hashset.contains(input.substring(j, j + i))) {
 						hashset.add(input.substring(j , j + i));
 					}
