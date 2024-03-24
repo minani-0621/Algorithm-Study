cf93119deae87ae4b6e95698b2367618c796ad82
조민환 <cm2hcm2h@naver.com>
2024-03-24 14:51:39 +0900

    [Bronze V] Title: A×B, Time: 216 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		int mul = a * b;
+		
+		System.out.println(mul);
+	}
+}
