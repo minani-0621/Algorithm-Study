cdc73e9c7dd7c3a13cc14271c360441f6166dd1d
조민환 <cm2hcm2h@naver.com>
2024-04-15 17:24:29 +0900

    [Silver V] Title: 지뢰 찾기, Time: 628 ms, Memory: 33008 KB -BaekjoonHub

--- 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.java
+++ 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.java
@@ -1,21 +1,22 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
 
 public class Main {
-	static Scanner scanner = new Scanner(System.in);
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static int n;
 	static char inputMap[][], resultMap[][];
 	
 	public static void main(String[] args) throws IOException {
-		n = scanner.nextInt();
+		n = Integer.parseInt(br.readLine());
 		inputMap = new char[n][n];
 		resultMap = new char[n][n];
 		
 		for(int i = 0; i < n; i++) {
-			String inputLine = scanner.next();
+			String inputLine = br.readLine();
 			for(int j = 0; j < n; j++) {
 				inputMap[i][j] = inputLine.charAt(j);
 			}
