d972edd8f3c7fb64a79088e42b71e3dd541eed0c
조민환 <cm2hcm2h@naver.com>
2024-04-24 08:58:58 +0900

    [Bronze V] Title: A+B - 2, Time: 204 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int result = a + b;
+		
+		System.out.println(result);
+	}
+}
