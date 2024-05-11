4ccc8a6a7490da91cf15f829dc8064db6f200611
조민환 <cm2hcm2h@naver.com>
2024-05-11 13:10:26 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		String input = scanner.next();
+		System.out.println(input + "??!");
+	}
+}
