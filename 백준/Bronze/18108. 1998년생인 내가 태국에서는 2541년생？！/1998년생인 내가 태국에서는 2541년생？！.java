f2aff858e8e2110866963328f00181a9c30aefaf
조민환 <cm2hcm2h@naver.com>
2024-05-23 22:30:50 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int y = scanner.nextInt();
+		System.out.println(y - 543);
+	}
+}
