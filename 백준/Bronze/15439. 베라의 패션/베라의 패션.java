623f4f5ce7017f7ab6a23fae1976036344a55ed7
조민환 <cm2hcm2h@naver.com>
2024-06-04 14:20:55 +0900

    [Bronze IV] Title: 베라의 패션, Time: 208 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int n = scanner.nextInt();
+		
+		System.out.println(n * (n - 1));
+	}
+}
