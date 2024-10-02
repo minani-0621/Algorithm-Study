2879438c1e3bcecd5cb6105c4eab21dc50849d02
조민환 <cm2hcm2h@naver.com>
2024-10-02 13:25:51 +0900

    [Silver V] Title: 커플링 매치, Time: 104 ms, Memory: 10288 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31908. 커플링 매치/커플링 매치.js
+++ 백준/Silver/31908. 커플링 매치/커플링 매치.js
@@ -2,7 +2,7 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
 
 const n = Number(input[0]);
-let studentMap = new Map();
+let ringMap = new Map();
 let coupleCount = 0;
 let answer = [];
 
@@ -10,18 +10,23 @@ for(let i = 1; i <= n; i++) {
     let [name, ring] = input[i].split(" ");
     
     if(ring !== "-") {
-        if(!studentMap.has(ring)) {
-            studentMap.set(ring, name);
-        }else {
-            answer.push(studentMap.get(ring));
-            answer.push(name);
-            coupleCount++;
+        if(!ringMap.has(ring)) {
+            ringMap.set(ring, []);
         }
+        
+        ringMap.get(ring).push(name);
     }
 }
 
+ringMap.forEach((name) => {
+    if(name.length === 2) {
+        coupleCount++;
+        answer.push(name.join(" "));
+    }
+})
+
 console.log(coupleCount);
 
-for(let i = 0; i < answer.length; i += 2) {
-    console.log(answer[i] + " " + answer[i + 1]);
+for(let i = 0; i < coupleCount; i++) {
+    console.log(answer[i]);
 }
