6618845b9782b12ebf14154dc4ddb55796372a1f
조민환 <cm2hcm2h@naver.com>
2025-01-06 23:00:48 +0900

    [Bronze V] Title: 홍익대학교, Time: 168 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int year = scanner.nextInt();
+		System.out.println(year - 1946);
+	}
+}
