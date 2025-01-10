d33b27bc1b2dab56d881460ad94007d1bfc5436c
조민환 <cm2hcm2h@naver.com>
2025-01-10 17:39:14 +0900

    [Bronze V] Title: AI 선도대학, Time: 184 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int N = scanner.nextInt();
+		System.out.println(N - 2024);
+	}
+}
