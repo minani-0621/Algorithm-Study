8479e3ca47956be03b4ad90258e20affb34ab63a
조민환 <cm2hcm2h@naver.com>
2025-01-16 22:01:37 +0900

    [Bronze V] Title: SciComLove, Time: 168 ms, Memory: 17460 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int N = scanner.nextInt();
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println("SciComLove");
+		}
+	}
+}
