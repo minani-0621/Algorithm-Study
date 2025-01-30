97c1bd1e7a1006d1479c9fecc58093b1950c0cc1
조민환 <cm2hcm2h@naver.com>
2025-01-30 21:46:48 +0900

    [Silver II] Title: 최대 힙, Time: 892 ms, Memory: 110816 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,33 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+	static Scanner scanner = new Scanner(System.in);
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws IOException {
+		int N = scanner.nextInt();
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		
+		for(int i = 0; i < N; i++) {
+			int command = scanner.nextInt();
+			
+			if(command == 0) {
+				if(pq.isEmpty()) {
+					bw.write("0\n");
+				}else {
+					bw.write(pq.poll() + "\n");
+				}
+			}else {
+				pq.add(command);
+			}
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
