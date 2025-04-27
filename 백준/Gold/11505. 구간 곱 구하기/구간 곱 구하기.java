81d3d8a6c3844577a49d348c8bf0c9d1682487db
조민환 <cm2hcm2h@naver.com>
2025-04-27 18:21:42 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 520 ms, Memory: 98820 KB -BaekjoonHub

---
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int N, M, K;
+	static long[] array, tree;
+	
+	public static void main(String[] args) throws IOException {
+		String[] input = br.readLine().split(" ");
+		N = Integer.parseInt(input[0]);
+		M = Integer.parseInt(input[1]);
+		K = Integer.parseInt(input[2]);
+		array = new long[N];
+		tree = new long[getTreeSize()];
+		
+		for(int i = 0; i < N; i++) {
+			array[i] = Long.parseLong(br.readLine());
+		}
+		
+		init(0, N - 1, 1);
+		
+		for(int i = 0; i < M + K; i++) {
+			String[] info = br.readLine().split(" ");
+			int a = Integer.parseInt(info[0]);
+			int b = Integer.parseInt(info[1]);
+			int c = Integer.parseInt(info[2]);
+			
+			if(a == 1) {
+				update(0, N - 1, 1, b - 1, c);
+			}else if(a == 2) {
+				long sum = partMultiply(0, N - 1, 1, b - 1, c - 1);
+				bw.write(sum + "\n");
+			}
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	static int getTreeSize() {
+		int h = (int)Math.ceil(Math.log(N) / Math.log(2));
+		return (int)Math.pow(2, h + 1);
+	}
+	
+	static long init(int start, int end, int treeIdx) {
+		if(start == end) {
+			return tree[treeIdx] = array[start];
+		}
+		
+		int mid = (start + end) / 2;
+		
+		return tree[treeIdx] = init(start, mid, treeIdx * 2) % 1000000007 * init(mid + 1, end, treeIdx * 2 + 1) % 1000000007;
+	}
+	
+	static void update(int start, int end, int treeIdx, int arrayIdx, long change) {		
+		if(start == end) {
+			tree[treeIdx] = change;
+			return;
+		}
+		
+		int mid = (start + end) / 2;
+		
+		if(arrayIdx <= mid) {
+			update(start, mid, treeIdx * 2, arrayIdx, change);
+		}else {
+			update(mid + 1, end, treeIdx * 2 + 1, arrayIdx, change);
+		}
+		
+		tree[treeIdx] = tree[treeIdx * 2] % 1000000007 * tree[treeIdx * 2 + 1] % 1000000007;
+	}
+	
+	static long partMultiply(int start, int end, int treeIdx, int l, int r) {
+		if(start > r || end < l) {
+			return 1;
+		}
+		
+		if(l <= start && end <= r) {
+			return tree[treeIdx];
+		}
+		
+		int mid = (start + end) / 2;
+		
+		return partMultiply(start, mid, treeIdx * 2, l, r) % 1000000007 * partMultiply(mid + 1, end, treeIdx * 2 + 1, l, r) % 1000000007;
+	}
+}
