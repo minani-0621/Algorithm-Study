38cb62edd941b4757342ab06dfb4280b86ada3ef
조민환 <cm2hcm2h@naver.com>
2025-05-24 19:38:21 +0900

    [Silver III] Title: N과 M (1), Time: 488 ms, Memory: 45412 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int N, M;
+	static ArrayList<Integer> sequence = new ArrayList<>();
+	static HashSet<Integer> set = new HashSet<>();
+	
+	public static void main(String[] args) throws IOException {
+		String[] input = br.readLine().split(" ");
+		N = Integer.parseInt(input[0]);
+		M = Integer.parseInt(input[1]);
+		
+		makeSequence(sequence);
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	static void makeSequence(ArrayList<Integer> seq) throws IOException {
+		if(seq.size() == M) {
+			for(int num: seq) {
+				bw.write(num + " ");
+			}
+			
+			bw.write("\n");
+			return;
+		}else {
+			for(int i = 1; i <= N; i++) {
+				if(!set.contains(i)) {
+					set.add(i);
+					seq.add(i);
+					makeSequence(seq);
+					set.remove(i);
+					seq.remove(seq.size() - 1);
+				}
+			}
+		}
+	}
+}
